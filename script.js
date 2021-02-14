aaa9844ede9757eff0dd56839c9a87a2db8afec9
ika776 <ikasariapriliyani@gmail.com>
2021-02-14 10:17:47 +0700

    toDoList

---
+++ script.js
@@ -0,0 +1,54 @@
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInp").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("demo").appendChild(li);
+    }
+    document.getElementById("myInp").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+
+    }
+  }
+  
+  
+  // Click on a close button to hide the current list item
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+  // Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('UL');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+  
+
+var d = new Date();
+var days = ["Ahad","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("time").innerHTML = days[d.getDay()] +", "+d.getDate()+" "+ months[d.getMonth()]+" "+d.getFullYear();
+
